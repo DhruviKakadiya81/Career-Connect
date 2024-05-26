78f16280f8e1afb6da468a08d48d89c3ce89e45c
mansirathod2319 <mansirathod763@gmail.com>
2024-05-26 14:19:31 +0530

    company patinet dashboard

--- src/main/java/ClientPC/Career_Connect_Client.java
+++ src/main/java/ClientPC/Career_Connect_Client.java
@@ -25,7 +25,7 @@ public class Career_Connect_Client {
 
     private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://localhost:8080//resources";
+    private static final String BASE_URI = "http://localhost:8080/Career-Connect/resources";
 
     public Career_Connect_Client() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
